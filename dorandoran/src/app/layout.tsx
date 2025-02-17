2bfb4be3ad3ee9d31c54184a62f2e3848c583417
hyunjihub <syub98774@naver.com>
2025-02-17 12:52:13 +0900

    [Init] Next.js 세팅 및 PWA 1차 세팅

---
+++ dorandoran/src/app/layout.tsx
@@ -0,0 +1,30 @@
+import './globals.css';
+
+import type { Metadata, Viewport } from 'next';
+
+export const viewport: Viewport = {
+  themeColor: 'black',
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  viewportFit: 'cover',
+};
+
+export const metadata: Metadata = {
+  title: '도란도란',
+  description: '도란도란에서 다양한 사람들과 실시간 채팅',
+  manifest: '/manifest.json',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
