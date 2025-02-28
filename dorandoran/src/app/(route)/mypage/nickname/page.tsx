4ee231f44b746a4a5070ea6cf83f5b9a8ef5cd9a
hyunjihub <syub98774@naver.com>
2025-02-28 17:20:08 +0900

    [fix] 배포를 위한 임시 페이지 구현

---
+++ dorandoran/src/app/(route)/mypage/nickname/page.tsx
@@ -0,0 +1,9 @@
+import CreateButton from '@/app/_component/chat/CreateButton';
+
+export default function ChatList() {
+  return (
+    <div className="h-full px-[16px] flex flex-col">
+      <CreateButton />
+    </div>
+  );
+}
