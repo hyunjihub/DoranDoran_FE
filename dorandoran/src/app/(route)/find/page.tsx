cbfc604252905e789c95a8be8e404c53d296cbc5
hyunjihub <syub98774@naver.com>
2025-03-02 17:01:45 +0900

    [fix] useSearchParams를 사용하는 페이지 suspense 추가

--- dorandoran/src/app/(route)/find/page.tsx
+++ dorandoran/src/app/(route)/find/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 
 import { AuthStatus } from '@/app/_util/types/types';
 import EmailInput from '@/app/_component/form/EmailInput';
@@ -21,15 +21,17 @@ export default function Find() {
   }, [queryType]);
 
   return (
-    <div className="h-full flex flex-col">
-      <FindHeader type={type} setType={setType} />
-      <form className="px-[16px] mt-[24px] flex flex-col gap-8">
-        <EmailInput authState={authState} setAuthState={setAuthState} />
-        {type === 'password' && <PasswordInput type="find" />}
-        <button className="w-full py-[12px] border border-[#7B3796] bg-[#7B3796] rounded text-white text-center font-bold">
-          {type === 'email' ? '이메일 찾기' : '비밀번호 찾기'}
-        </button>
-      </form>
-    </div>
+    <Suspense>
+      <div className="h-full flex flex-col">
+        <FindHeader type={type} setType={setType} />
+        <form className="px-[16px] mt-[24px] flex flex-col gap-8">
+          <EmailInput authState={authState} setAuthState={setAuthState} />
+          {type === 'password' && <PasswordInput type="find" />}
+          <button className="w-full py-[12px] border border-[#7B3796] bg-[#7B3796] rounded text-white text-center font-bold">
+            {type === 'email' ? '이메일 찾기' : '비밀번호 찾기'}
+          </button>
+        </form>
+      </div>
+    </Suspense>
   );
 }
