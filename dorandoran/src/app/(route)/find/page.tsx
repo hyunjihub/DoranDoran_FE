00a83eb96de4274c977ef8770fb4f05252dd2895
hyunjihub <syub98774@naver.com>
2025-03-02 17:09:46 +0900

    [fix] FindForm 컴포넌트 분리

--- dorandoran/src/app/(route)/find/page.tsx
+++ dorandoran/src/app/(route)/find/page.tsx
@@ -1,37 +1,9 @@
-'use client';
-
-import { Suspense, useEffect, useState } from 'react';
-
-import { AuthStatus } from '@/app/_util/types/types';
-import EmailInput from '@/app/_component/form/EmailInput';
-import FindHeader from '@/app/_component/user/FindHeader';
-import PasswordInput from '@/app/_component/form/PasswordInput';
-import { useSearchParams } from 'next/navigation';
+import FindForm from '@/app/_component/user/FindForm';
 
 export default function Find() {
-  const searchParams = useSearchParams();
-  const queryType = searchParams.get('type');
-  const [type, setType] = useState(queryType === 'password' ? 'password' : 'email');
-  const [authState, setAuthState] = useState<AuthStatus>('idle');
-
-  useEffect(() => {
-    if (queryType === 'password' || queryType === 'email') {
-      setType(queryType);
-    }
-  }, [queryType]);
-
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div className="h-full flex flex-col">
-        <FindHeader type={type} setType={setType} />
-        <form className="px-[16px] mt-[24px] flex flex-col gap-8">
-          <EmailInput authState={authState} setAuthState={setAuthState} />
-          {type === 'password' && <PasswordInput type="find" />}
-          <button className="w-full py-[12px] border border-[#7B3796] bg-[#7B3796] rounded text-white text-center font-bold">
-            {type === 'email' ? '이메일 찾기' : '비밀번호 찾기'}
-          </button>
-        </form>
-      </div>
-    </Suspense>
+    <div className="h-full flex flex-col">
+      <FindForm />
+    </div>
   );
 }
---
+++ dorandoran/src/app/_component/user/FindForm.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+import { AuthStatus } from '@/app/_util/types/types';
+import EmailInput from '@/app/_component/form/EmailInput';
+import FindHeader from '@/app/_component/user/FindHeader';
+import PasswordInput from '@/app/_component/form/PasswordInput';
+import { useSearchParams } from 'next/navigation';
+
+export default function FindForm() {
+  const searchParams = useSearchParams();
+  const queryType = searchParams.get('type');
+  const [type, setType] = useState(queryType === 'password' ? 'password' : 'email');
+  const [authState, setAuthState] = useState<AuthStatus>('idle');
+
+  useEffect(() => {
+    if (queryType === 'password' || queryType === 'email') {
+      setType(queryType);
+    }
+  }, [queryType]);
+
+  return (
+    <>
+      <FindHeader type={type} setType={setType} />
+      <form className="px-[16px] mt-[24px] flex flex-col gap-8">
+        <EmailInput authState={authState} setAuthState={setAuthState} />
+        {type === 'password' && <PasswordInput type="find" />}
+        <button className="w-full py-[12px] border border-[#7B3796] bg-[#7B3796] rounded text-white text-center font-bold">
+          {type === 'email' ? '이메일 찾기' : '비밀번호 찾기'}
+        </button>
+      </form>
+    </>
+  );
+}
