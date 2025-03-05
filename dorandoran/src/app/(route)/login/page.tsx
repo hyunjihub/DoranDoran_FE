7cd078b63f85fc3a1435bb8032c2fa5f37328bbd
hyunjihub <syub98774@naver.com>
2025-03-05 14:49:29 +0900

    [fix] login useSearchParam 사용에 따른 빌드 오류 해결

--- dorandoran/src/app/(route)/login/page.tsx
+++ dorandoran/src/app/(route)/login/page.tsx
@@ -1,20 +1,10 @@
-import Link from 'next/link';
-import LoginForm from '@/app/_component/user/LoginForm';
+import LoginComponent from '@/app/_component/user/LoginComponent';
+import { Suspense } from 'react';
 
 export default function Login() {
   return (
-    <div className="h-full px-[16px] flex flex-col">
-      <LoginForm />
-      <Link className="mt-[8px] text-center text-xs" href={'/find'}>
-        아이디 | 비밀번호 찾기
-      </Link>
-      <Link
-        className="mt-[40px] w-full py-[12px] border border-[#7B3796] rounded text-[#7B3796] text-center font-bold"
-        href={'/signup'}
-        role="button"
-      >
-        회원가입
-      </Link>
-    </div>
+    <Suspense>
+      <LoginComponent />
+    </Suspense>
   );
 }
---
+++ dorandoran/src/app/_component/user/LoginComponent.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import LoginForm from '@/app/_component/user/LoginForm';
+
+export default function LoginComponent() {
+  return (
+    <div className="h-full px-[16px] flex flex-col">
+      <LoginForm />
+      <Link className="mt-[8px] text-center text-xs" href={'/find'}>
+        아이디 | 비밀번호 찾기
+      </Link>
+      <Link
+        className="mt-[40px] w-full py-[12px] border border-[#7B3796] rounded text-[#7B3796] text-center font-bold"
+        href={'/signup'}
+        role="button"
+      >
+        회원가입
+      </Link>
+    </div>
+  );
+}
