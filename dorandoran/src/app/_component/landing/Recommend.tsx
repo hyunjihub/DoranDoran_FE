7e92345fe6fc293c824f0b74269480a024e9f8e5
hyunjihub <syub98774@naver.com>
2025-03-20 01:59:49 +0900

    [fix] 임시 로그인 삭제

--- dorandoran/src/app/_component/landing/Recommend.tsx
+++ dorandoran/src/app/_component/landing/Recommend.tsx
@@ -1,26 +1,10 @@
 'use client';
-
-import { useStore } from '@/store/useStore';
-
 export default function Recommend() {
-  const { login } = useStore();
-
-  const handleLogin = () => {
-    login({
-      userId: 1,
-      profileImg: null,
-      nickname: '임시로그인',
-    });
-  };
-
   return (
     <section className="mt-[60px] px-[24px]">
       <h2 className="text-xl font-bold">새로운 친구를 만나보세요! ️👭</h2>
       <p className="text-xs text-gray-500">새로운 친구, 새로운 인연! 도란도란에서!</p>
-      <button
-        className="mt-4 px-4 py-2 bg-blue-500 text-white text-sm font-semibold rounded-lg hover:bg-blue-600"
-        onClick={handleLogin}
-      >
+      <button className="mt-4 px-4 py-2 bg-blue-500 text-white text-sm font-semibold rounded-lg hover:bg-blue-600">
         임시 로그인
       </button>
     </section>
