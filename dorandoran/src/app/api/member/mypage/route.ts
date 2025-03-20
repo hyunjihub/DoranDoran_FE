2b3a47b10903c375f0a7a887f5f89c707fde94e1
hyunjihub <syub98774@naver.com>
2025-03-21 02:34:53 +0900

    [fix] mypage 토큰 검사 코드 임시 삭제

--- dorandoran/src/app/api/member/mypage/route.ts
+++ dorandoran/src/app/api/member/mypage/route.ts
@@ -1,15 +1,9 @@
 import { IMypage } from '@/app/_util/types/types';
 import { NextResponse } from 'next/server';
 import axios from 'axios';
-import { checkTokens } from '@/app/_util/tokenCheck';
 
 export async function POST() {
   try {
-    const tokenErrorResponse = await checkTokens();
-    if (tokenErrorResponse) {
-      return tokenErrorResponse;
-    }
-
     const { data } = await axios.post<IMypage>(
       `${process.env.API_BASE_URL}/member/mypage`,
       {},
