33c45dd46645e0f635fc211f9e9931d4d2059271
hyunjihub <syub98774@naver.com>
2025-03-21 02:15:44 +0900

    [feat] 서버 사이드 쿠키 설정

--- dorandoran/src/app/api/member/login/route.ts
+++ dorandoran/src/app/api/member/login/route.ts
@@ -1,20 +1,40 @@
 import { IUser } from '@/app/_util/types/types';
 import { NextResponse } from 'next/server';
 import axios from 'axios';
+import { cookies } from 'next/headers';
 
 export async function POST(req: Request) {
   try {
     const body = await req.json();
     const host = req.headers.get('host') || 'www.dorandoran.online';
 
-    const { data } = await axios.post<IUser>(`${process.env.API_BASE_URL}/member/login`, body, {
+    const response = await axios.post<IUser>(`${process.env.API_BASE_URL}/member/login`, body, {
       withCredentials: true,
       headers: {
         Host: host,
         'Content-Type': 'application/json',
       },
     });
-    return NextResponse.json(data, { status: 201 });
+
+    const setCookieHeader = response.headers['set-cookie'];
+    if (setCookieHeader) {
+      setCookieHeader.forEach(async (cookie) => {
+        const [cookieName, cookieValue] = cookie.split(';')[0].split('=');
+        (await cookies()).set(cookieName, cookieValue, {
+          httpOnly: true,
+          secure: true,
+          sameSite: 'none',
+          path: '/',
+          domain: '.dorandoran.online',
+        });
+      });
+    }
+
+    const res = NextResponse.json(response.data, { status: 201 });
+    if (setCookieHeader) {
+      res.headers.set('Set-Cookie', setCookieHeader.join(', '));
+    }
+    return res;
   } catch (error: unknown) {
     let errorMessage = '서버 오류 발생';
     let status = 500;
