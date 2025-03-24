ddc6ae6c9fdc19300fe6fe480756dd6e0df3da0c
hyunjihub <syub98774@naver.com>
2025-03-24 19:44:14 +0900

    [fix] 프록시 요청 시 method 변경

--- dorandoran/src/app/_util/getFetchUserInfo.ts
+++ dorandoran/src/app/_util/getFetchUserInfo.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const getFetchUserInfo = async (): Promise<IMypage> => {
   try {
-    const response = await axios.post('/api/member/mypage');
+    const response = await axios.get('/api/member/mypage');
     return response.data;
   } catch (error) {
     if (axios.isAxiosError(error)) {
