5c3c1b9e75798f9147d581df557928289ee59a97
hyunjihub <syub98774@naver.com>
2025-05-15 15:25:10 +0900

    [feat] 내 채팅 불러오기 route.ts 구현

---
+++ dorandoran/src/app/api/chat/lists/route.tsx
@@ -0,0 +1,30 @@
+import { IRoom } from '@/app/_util/types/types';
+import { NextResponse } from 'next/server';
+import axios from 'axios';
+import { cookies } from 'next/headers';
+
+export async function GET() {
+  try {
+    const cookieStore = cookies();
+    const accessToken = (await cookieStore).get('access')?.value || '';
+
+    const { data } = await axios.get<IRoom>(`${process.env.API_BASE_URL}/chat/lists`, {
+      withCredentials: true,
+      headers: {
+        Cookie: `access=${accessToken}`,
+      },
+    });
+
+    return NextResponse.json(data, { status: 201 });
+  } catch (error: unknown) {
+    let errorMessage = '서버 오류 발생';
+    let status = 500;
+
+    if (axios.isAxiosError(error) && error.response) {
+      errorMessage = error.response.data?.message || errorMessage;
+      status = error.response.status || status;
+    }
+
+    return NextResponse.json({ error: errorMessage }, { status });
+  }
+}
