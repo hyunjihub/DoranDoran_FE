c073bd20f6854a7e530cf408ff1b7c81ef6aaae9
hyunjihub <syub98774@naver.com>
2025-05-27 19:44:20 +0900

    [design] 채팅 날짜 font-weight 변경, 개인 채팅방 헤더 수정 작업 중

--- dorandoran/src/app/_component/chat/Date.tsx
+++ dorandoran/src/app/_component/chat/Date.tsx
@@ -1,5 +1,7 @@
 export default function Date({ date }: { date: string }) {
   return (
-    <div className="w-[150px] text-center text-xs text-gray-700 bg-gray-200 rounded-xl px-4 py-1 my-3">{date}</div>
+    <div className="w-[150px] text-center text-xs font-semibold text-gray-700 bg-gray-200 rounded-xl px-4 py-1 my-3">
+      {date}
+    </div>
   );
 }
--- dorandoran/src/app/_component/layout/header/ChatHeader.tsx
+++ dorandoran/src/app/_component/layout/header/ChatHeader.tsx
@@ -6,19 +6,26 @@ import Image from 'next/image';
 import Link from 'next/link';
 import arrow from '/public/img/icon/prevArrow.svg';
 import { chatStore } from '@/store/useChatStore';
+import { isMobile } from 'react-device-detect';
 import more from '/public/img/icon/more.svg';
+import { useState } from 'react';
 import { websocketStore } from '@/store/useWebsocketStore';
 
 export default function ChatHeader() {
   const unsubscribeRoom = websocketStore((state) => state.unsubscribeRoom);
   const chatTitle = chatStore((state) => state.chatTitle);
-
   const pathname = usePathname();
   const router = useRouter();
+
+  const [showDropdown, setShowDropdown] = useState(false);
 
   const handleGoToChatList = () => {
     unsubscribeRoom();
     router.push('/mychat');
+  };
+
+  const handleToggleDropdown = () => {
+    setShowDropdown((prev) => !prev);
   };
 
   return (
@@ -26,11 +33,26 @@ export default function ChatHeader() {
       <div className="absolute left-[16px]" onClick={handleGoToChatList} role="button">
         <Image src={arrow} alt="이전페이지" width={12} height={24} />
       </div>
+
       <h1 className="font-semibold">{chatTitle}</h1>
+
       {!pathname.includes('info') && (
-        <Link className="absolute right-[16px]" href={`${pathname}/info`} role="button">
-          <Image src={more} alt="채팅방 설정" width={32} height={32} />
-        </Link>
+        <>
+          {!isMobile ? (
+            <div className="absolute right-[16px] z-50" role="button" onClick={handleToggleDropdown}>
+              <Image src={more} alt="메뉴 열기" width={32} height={32} />
+              {showDropdown && (
+                <ul className="absolute top-full right-0 w-[100px] bg-white border rounded shadow z-10">
+                  <li className="w-full px-2 py-2 hover:bg-gray-100">채팅방 나가기</li>
+                </ul>
+              )}
+            </div>
+          ) : (
+            <Link className="absolute right-[16px]" href={`${pathname}/info`} role="button">
+              <Image src={more} alt="채팅방 설정" width={32} height={32} />
+            </Link>
+          )}
+        </>
       )}
     </div>
   );
