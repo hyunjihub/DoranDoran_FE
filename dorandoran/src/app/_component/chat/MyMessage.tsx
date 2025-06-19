643e1388735f66670eabe24db6f4c3d0717ed20c
hyunjihub <syub98774@naver.com>
2025-06-19 22:01:39 +0900

    [feat] text 내 링크가 포함된 경우 Link 기능 부여

--- dorandoran/src/app/_component/chat/MyMessage.tsx
+++ dorandoran/src/app/_component/chat/MyMessage.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 interface MyMessageProps {
   type: 'text' | 'image' | 'system';
@@ -7,13 +8,21 @@ interface MyMessageProps {
 }
 
 export default function MyMessage({ type, message, timestamp }: MyMessageProps) {
+  const isLink = /^https?:\/\/\S+$/.test(message);
+
   return (
     <div className="w-full flex justify-end">
       <div className="flex gap-1.5 items-end my-2">
         {timestamp && <p className="text-xs text-gray-400 font-light">{timestamp}</p>}
         <div className="max-w-[200px] bg-[#7B3796] text-white rounded-lg py-1.5 px-3 text-sm">
           {type === 'text' ? (
-            <p>{message}</p>
+            isLink ? (
+              <p>{message}</p>
+            ) : (
+              <Link href={message} target="_blank" rel="noopener noreferrer" className="underline text-blue-200">
+                {message}
+              </Link>
+            )
           ) : (
             <Image className="py-1" src={message} alt="이미지" width={180} height={0} unoptimized />
           )}
