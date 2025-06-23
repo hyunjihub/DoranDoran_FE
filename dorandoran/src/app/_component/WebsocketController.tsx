fb68f25d2e0298b887ccc49e6b9ba06fab12314f
hyunjihub <syub98774@naver.com>
2025-06-23 15:55:00 +0900

    [fix] websocket 연결이 유지되지 않는 오류 수정

--- dorandoran/src/app/_component/WebsocketController.tsx
+++ dorandoran/src/app/_component/WebsocketController.tsx
@@ -9,20 +9,13 @@ export default function WebSocketController() {
   const isLoggedIn = userStore((state) => state.isLoggedIn);
   const pathname = usePathname();
   const connect = websocketStore((state) => state.connect);
-  const disconnect = websocketStore((state) => state.disconnect);
   const socket = websocketStore((state) => state.socket);
 
   useEffect(() => {
     if (isLoggedIn && !socket) {
       connect();
     }
-
-    return () => {
-      if (socket) {
-        disconnect();
-      }
-    };
-  }, [isLoggedIn, socket, pathname, disconnect, connect]);
+  }, [isLoggedIn, socket, pathname, connect]);
 
   return null;
 }
